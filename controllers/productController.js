d738357aa5b0007ff9e99d785eaed6a9b1ec24fb
longhoang <[hoalanh21112002@gmail.com]>
2023-08-10 20:43:01 +0700

    implement aggregate features

--- controllers/productController.js
+++ controllers/productController.js
@@ -33,4 +33,84 @@ const aliasTop6PremiumProduct = (req, _, next) => {
   next();
 };
 
-module.exports = { getAllProducts, aliasTop6PremiumProduct };
+//Middleware format request catefory
+const formatBrandReq = (req, res, next) => {
+  req.params.brand =
+    req.params.brand.charAt(0).toUpperCase() + req.params.brand.slice(1);
+  // req.params.brand = req.params.brand.replace(
+  //   req.params.brand.charAt(0),
+  //   req.params.brand.charAt(0).toUpperCase(),
+  // );
+  next();
+};
+
+const checkReq = (req, res, next) => {
+  req.body = "ok";
+  next();
+};
+//GET STATISTICS OF PHONE BASED ON BRAND, CATEGORY
+const getProductStats = async (req, res) => {
+  try {
+    const { brand, category } = req.params;
+    const stats = await Product.aggregate([
+      // {
+      //   $match: { brand: `${brand}`, category: `${category}` },
+      // },
+      {
+        $group: {
+          _id: { brand: `${brand}`, category: `${category}` },
+          avgPrice: { $avg: "$price" },
+          avgRating: { $avg: "$rating" },
+          avgDiscount: { $avg: "$discountPercentage" },
+          maxPrice: { $max: "$price" },
+          minPrice: { $min: "$price" },
+          maxRating: { $max: "$rating" },
+          minRating: { $min: "$rating" },
+          maxDiscount: { $max: "$discountPercentage" },
+          minDiscount: { $min: "$discountPercentage" },
+          totalProduct: { $sum: 1 },
+          totalPrice: { $sum: "$price" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          info: "$_id",
+          avgPrice: 1,
+          avgRating: 1,
+          avgDiscount: 1,
+          maxPrice: 1,
+          minPrice: 1,
+          maxRating: 1,
+          minRating: 1,
+          maxDiscount: 1,
+          minDiscount: 1,
+          totalProduct: 1,
+          totalPrice: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: "Success",
+      results: stats.length,
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Fails",
+      message: "Can'\t request",
+      error: err.message,
+    });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  aliasTop6PremiumProduct,
+  getProductStats,
+  formatBrandReq,
+  checkReq,
+};
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,13 +2,22 @@ const express = require("express");
 const {
   getAllProducts,
   aliasTop6PremiumProduct,
+  getProductStats,
+  formatBrandReq,
+  checkReq,
 } = require("../controllers/productController");
 
 const router = express.Router();
+// router.use(formatBrandReq);//why your put middleware it's not work because in this time we don't catch to route /products-stats/:brand/:category so brand and catefory not defined so it's undefind and return error
+// router.use(checkReq);
 
 router
   .route("/top-6-premium-products")
   .get(aliasTop6PremiumProduct, getAllProducts);
+
+router
+  .route("/products-stats/:brand/:category")
+  .get(formatBrandReq, getProductStats); // you should give it here
 
 router.route("/").get(getAllProducts);
 
